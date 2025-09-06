0dcd46f4e800827b9738dddce7589e1171fdabbd
Nguyễn Thành Đạt <datnguyen9240@gmail.com>
2025-09-06 23:08:23 +0700

    fix: add overflow

--- app/products/category/[id]/layout.tsx
+++ app/products/category/[id]/layout.tsx
@@ -1,7 +1,7 @@
 import FilterSidebar from "@/app/components/FilterSidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex min-h-screen max-w-[1300px] mx-auto">
+  <div className="flex min-h-screen max-w-[1300px] mx-auto overflow-hidden">
     <FilterSidebar className="hidden lg:block" />
     <main className="flex-1 p-8">{children}</main>
   </div>
