af0e34c4d45515f2f1418cb472567d060365f64a
Nguyen Thanh Dat <datnguyen9240@gmail.com>
2026-01-31 00:43:20 +0700

    fix: badge

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon || '/logo/01.png',
+    icon: payload.notification.icon || '/icon.png',
     badge: payload.notification.badge || '/badge/01.png',
     data: payload.data
   };
